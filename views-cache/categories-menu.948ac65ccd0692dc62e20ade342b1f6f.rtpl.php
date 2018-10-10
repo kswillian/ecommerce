ec5b14721e0a2e59b1537e7a59e8f5fb2e7d322d
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2018-10-10 00:28:59 -0300

    Categorias VS Produtos

--- views-cache/categories-menu.948ac65ccd0692dc62e20ade342b1f6f.rtpl.php
+++ views-cache/categories-menu.948ac65ccd0692dc62e20ade342b1f6f.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/2">Eletrodomésticos</a></li><li><a href="/categories/3">Eletronicos</a></li><li><a href="/categories/1">Moda e Acessórios</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/5">Android</a></li><li><a href="/categories/2">Eletrodomésticos</a></li><li><a href="/categories/3">Eletronicos</a></li><li><a href="/categories/1">Moda e Acessórios</a></li>
