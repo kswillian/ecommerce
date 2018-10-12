9c195505d9be0d8b686da1cb9b09301e55c3ae25
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2018-10-12 14:26:16 -0300

    carrinho de compras

--- vendor/hcodebr/php-classes/src/Model.php
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -1,41 +1,50 @@
-<?php
+<?php 
 
 namespace Hcode;
 
-class Model{
+class Model {
 
 	private $values = [];
 
-	public function __call($name, $args){
+	public function __call($name, $args)
+	{
 
 		$method = substr($name, 0, 3);
-		$fieldname = substr($name, 3, strlen($name));
+		$fieldName = substr($name, 3, strlen($name));
 
-		switch ($method) {
-			case 'get':
-				return (isset($this->values[$fieldname])) ? $this->values[$fieldname] : NULL;
+		switch ($method)
+		{
+
+			case "get":
+				return (isset($this->values[$fieldName])) ? $this->values[$fieldName] : NULL;
 			break;
-			
-			case 'set':
-				$this->values[$fieldname] = $args[0];
+
+			case "set":
+				$this->values[$fieldName] = $args[0];
 			break;
+
 		}
+
 	}
 
-	public function setData($data = array()){
+	public function setData($data = array())
+	{
 
 		foreach ($data as $key => $value) {
 			
-			$this->{"set" . $key}($value);
+			$this->{"set".$key}($value);
 
 		}
+
 	}
 
-	public function getValues(){
+	public function getValues()
+	{
 
 		return $this->values;
 
 	}
+
 }
 
-?>
+ ?>
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -11,6 +11,50 @@ class User extends Model{
 	const SESSION = "User";
 	const SECRET = "#Php7_@@_Secret#";
 
+	public static function getFromSession(){
+
+		$user = new User();
+
+		if(isset($_SESSION[User::SESSION]) && (int)$_SESSION[User::SESSION]["iduser"] > 0){
+
+			$user->setData($_SESSION[User::SESSION]);
+		}
+
+		return $user;
+	}
+
+	public static function checkLogin($inadmin = true){
+
+		if(
+			!isset($_SESSION[User::SESSION]) 
+			|| 
+			!$_SESSION[User::SESSION] 
+			|| 
+			!(int)$_SESSION[User::SESSION]["iduser"] > 0 
+		){
+
+			// Não esta logado
+			return false;
+
+		}else{
+
+			if($inadmin === true && (bool)$_SESSION[User::SESSION]['inadmin']){
+
+				return true;
+
+			}else if($inadmin === false){
+
+				return true;
+			
+			}else{
+
+				return false;
+			}
+
+		}
+
+	}
+
 	public static function login($login, $password){
 
 		$sql = new Sql();
@@ -44,17 +88,11 @@ class User extends Model{
 
 	public static function verifyLogin($inadmin = true){
 
-		if(
-			!isset($_SESSION[User::SESSION]) 
-			|| 
-			!$_SESSION[User::SESSION] 
-			|| 
-			!(int)$_SESSION[User::SESSION]["iduser"] > 0 
-			||
-			(bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
-		){
+		if(!User::checkLogin($inadmin)){
+			
 			header("Location: /admin/login");
 			exit;
+		
 		}
 	}
 
